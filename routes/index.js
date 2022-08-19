adffa2e75aad8adb666ce13d05fcd1fe5b152fcc
jwood7 <jwood7@stevens.edu>
2022-08-19 14:03:20 -0400

    Initial Commit
    Basic expressJS set up with handlebars

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const codeRoutes = require("./code");
+const gamesRoutes = require("./games");
+const artRoutes = require("./art");
+
+const path = require("path");
+
+const constructorMethod = app => {
+  app.use("/code", codeRoutes);
+  app.use("/games", gameRoutes);
+  app.use("/art", artRoutes);
+
+  app.use("*", (req, res) => {
+    res.status(200).render('pages/home');
+
+    // res.status(num).render(template, data)
+    // path.resolve(`path/fileName.html`); -> res.sendfile
+  });
+};
+
+module.exports = constructorMethod;
